8b1f6e1ad8d0c75f0f55f4e640004585b7886f68
Giov25 <patane.giovanni@phd.unict.it>
2026-01-06 14:18:39 +0000

    AIA_to_zarr e segmentation script

--- AIA_to_zarr_file.py
+++ AIA_to_zarr_file.py
@@ -64,19 +64,98 @@ def resize_to_match_solar_radius(image_data, current_radius, target_radius, padd
         return cropped_data, scale_factor
 
 def estrai_wavelenght(file_name):
-    stringa = file_name[23:26]  
-    num = re.findall(r'\d+', stringa)
-    return num[0] if num else ""
+    wl = file_name.split(']')[1].split('[')[1]
+    return wl
+import os
+import re
+import warnings
+import argparse
+import numpy as np
+from tqdm import tqdm
+import zarr
+from numcodecs import Blosc
+from sunpy.io._file_tools import read_file_header 
+from sunpy.map import Map
+import skimage.transform
+from astropy import units as u
+import astropy.time
+from aiapy.calibrate import degradation
+from aiapy.calibrate.util import get_correction_table
+from astropy.io import fits
+
+def estrai_wavelenght(file_name):
+    wl = file_name.split(']')[1].split('[')[1]
+    return wl
+def create_synchronized_filelists_samefolder(folder_path, year):
+    """Create lists of files that have matching observation dates across all three data sources"""
+    year_str = str(year)
+    aia_list = ['131', '171', '193', '211', '304', '335', '94', '1600', '1700']
+    aia_files_by_date = {wavelength: {} for wavelength in aia_list}
+    M_files_by_date = {}
+    ic_nl_files_by_date = {}
+    for file in sorted(os.listdir(folder_path)):
+        if file[1:5] in year_str:
+            wl = estrai_wavelenght(file)
+            if wl in aia_list:
+                try:
+                    Xh = read_file_header(os.path.join(folder_path, file))
+                    if Xh[1]['QUALITY'] == 0:
+                        date_str = file.split("[")[1].split("]")[0][:13]
+                        if date_str not in aia_files_by_date[wl]:
+                            aia_files_by_date[wl][date_str] = []
+                        aia_files_by_date[wl][date_str].append(os.path.join(folder_path, file))
+                except:
+                    print(f"FILE CORRUPTED: {file}")
+                    continue
+            elif wl == 'M':
+                date_str = file.split("[")[1].split("]")[0][:13]
+                if date_str not in M_files_by_date:
+                    M_files_by_date[date_str] = []
+                M_files_by_date[date_str].append(os.path.join(folder_path, file))
+            elif wl == 'Ic':
+                date_str = file.split("[")[1].split("]")[0][:13]
+                if date_str not in ic_nl_files_by_date:
+                    ic_nl_files_by_date[date_str] = []
+                ic_nl_files_by_date[date_str].append(os.path.join(folder_path, file))
+    # Find common dates
+    if all(aia_files_by_date[wavelength] for wavelength in aia_files_by_date):
+        wavelengths = list(aia_files_by_date.keys())
+        common_aia_dates = set(aia_files_by_date[wavelengths[0]].keys())
+        
+        for wavelength in wavelengths[1:]:
+            common_aia_dates = common_aia_dates.intersection(set(aia_files_by_date[wavelength].keys()))
+
+        common_dates = common_aia_dates.intersection(set(M_files_by_date.keys()), set(ic_nl_files_by_date.keys()))
+
+    else:
+        print("Warning: Some wavelengths have no data")
+        common_dates = set()
+    # Filter files to only include common dates
+    filelist_131 = [f for date in common_dates for f in aia_files_by_date['131'].get(date, [])]
+    filelist_171 = [f for date in common_dates for f in aia_files_by_date['171'].get(date, [])]
+    filelist_193 = [f for date in common_dates for f in aia_files_by_date['193'].get(date, [])]
+    filelist_211 = [f for date in common_dates for f in aia_files_by_date['211'].get(date, [])]
+    filelist_304 = [f for date in common_dates for f in aia_files_by_date['304'].get(date, [])]
+    filelist_335 = [f for date in common_dates for f in aia_files_by_date['335'].get(date, [])]
+    filelist_94 = [f for date in common_dates for f in aia_files_by_date['94'].get(date, [])]
+    filelist_1600 = [f for date in common_dates for f in aia_files_by_date['1600'].get(date, [])]
+    filelist_1700 = [f for date in common_dates for f in aia_files_by_date['1700'].get(date, [])]
+    M_list = [f for date in common_dates for f in M_files_by_date.get(date, [])]
+    Ic_noLimbDark = [f for date in common_dates for f in ic_nl_files_by_date.get(date, [])]
+    return M_list, Ic_noLimbDark, filelist_131, filelist_171, filelist_193, filelist_211, filelist_304, filelist_335, filelist_94, filelist_1600, filelist_1700
+
 
 def create_synchronized_filelists(aia_folder_path, M_folder_path, ic_nl_folder_path, year, args):
     """Create lists of files that have matching observation dates across all three data sources"""
     year_str = str(year)
+
     
     # Initialize dictionaries to store files by date
     aia_files_by_date = {wavelength: {} for wavelength in ['131', '171', '193', '211', '304', '335', '94', '1600', '1700']}
     M_files_by_date = {}
     ic_nl_files_by_date = {}
-    
+
+
     # Process AIA files
     for file in sorted(os.listdir(aia_folder_path)):
         if file[1:5] == year_str:
@@ -163,23 +242,19 @@ def get_avg_degrad(anno):
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Process AIA files and generate Zarr datasets.")
-    parser.add_argument("--folder_path", type=str, default="/home/gpatane/Dataset/AIA_folder", help="Path to the folder containing AIA files")
-    parser.add_argument("--M_folder_path", type=str, default="/home/gpatane/Dataset/Magnetogram", help="Path to the folder containing IC files")
-    parser.add_argument("--IC_folder_path_nl", type=str, default="/home/gpatane/Dataset/Ic_noLimbDark", help="Path to the folder containing IC files no Limb")
-    parser.add_argument('--output_folder', type=str, default="/home/gpatane/Dataset/zarr_file_magnetogram_4096.zarr")
+    parser.add_argument("--folder_path", type=str, default="/home/gpatane/Dataset/AIA_IC_M_folder/", help="Path to the folder containing AIA files")
+    parser.add_argument('--output_folder', type=str, default="/home/gpatane/Dataset/zarr_file_magnetogram_1024_definitivo.zarr")
     parser.add_argument('--overwrite', action='store_true', default=False, help='Allow existing output folder without raising an error')
-    parser.add_argument('--start_period', type=int, required=False, default=2024)
+    parser.add_argument('--start_period', type=int, required=False, default=2011)
     parser.add_argument('--end_period', type=int, required=False, default=2026)
-    parser.add_argument('--image_size', type=int, required=False, default=4096, help='Size of the images to process')
-    parser.add_argument('--do_M_list', action='store_true', default=True, help='Whether to process Magnetogram files')
+    parser.add_argument('--image_size', type=int, required=False, default=1024, help='Size of the images to process')
+    parser.add_argument('--do_M_list', action='store_true', default=False, help='Whether to process Magnetogram files')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
     folder_path = args.folder_path
     years = list(range(args.start_period, args.end_period))
-    M_folder_path = args.M_folder_path
-    IC_folder_path_nl = args.IC_folder_path_nl
     output_folder = args.output_folder
     
     os.makedirs(output_folder, exist_ok=True)
@@ -188,13 +263,12 @@ if __name__ == "__main__":
     root = zarr.group(store=store, overwrite=args.overwrite)
     # Target radius from AIA data (approximately)
     radius_ratio = 0.3865
-    TARGET_RADIUS = args.image_size * radius_ratio
+    #TARGET_RADIUS = args.image_size * radius_ratio
+    TARGET_RADIUS = 4096 * radius_ratio
+    
     for year in years:
         print(f"Processing year {year}...")
-        if args.do_M_list:
-            M_list, Ic_noLimbDark, filelist_131, filelist_171, filelist_193, filelist_211, filelist_304, filelist_335, filelist_94, filelist_1600, filelist_1700 = create_synchronized_filelists(folder_path, M_folder_path, IC_folder_path_nl, year, args)
-        else:
-            _, Ic_noLimbDark, filelist_131, filelist_171, filelist_193, filelist_211, filelist_304, filelist_335, filelist_94, filelist_1600, filelist_1700 = create_synchronized_filelists(folder_path, M_folder_path, IC_folder_path_nl, year, args)
+        M_list, Ic_noLimbDark, filelist_131, filelist_171, filelist_193, filelist_211, filelist_304, filelist_335, filelist_94, filelist_1600, filelist_1700 = create_synchronized_filelists_samefolder(folder_path, year)
         
         if not Ic_noLimbDark:
             print(f"No data found for year {year}, skipping...")
@@ -214,12 +288,8 @@ if __name__ == "__main__":
         #divideFactor = int(1024 / scale)
         
         # Create datasets
-        if args.do_M_list:
-            M = year_group.create_dataset('Magnetogram', shape=(len(M_list), scale, scale), chunks=(15, None, None), dtype='f4', compressor=compressor)
-        else:
-            print("Skipping Magnetogram dataset creation as do_M_list is False")
-        if Ic_noLimbDark:
-            Ic_nl = year_group.create_dataset('Ic_noLimbDark', shape=(len(Ic_noLimbDark), scale, scale), chunks=(15, None, None), dtype='f4', compressor=compressor)
+        M = year_group.create_dataset('Magnetogram', shape=(len(M_list), scale, scale), chunks=(15, None, None), dtype='f4', compressor=compressor)
+        Ic_nl = year_group.create_dataset('Ic_noLimbDark', shape=(len(Ic_noLimbDark), scale, scale), chunks=(15, None, None), dtype='f4', compressor=compressor)
         
         # Create AIA datasets
         aia_datasets = {}
@@ -264,6 +334,8 @@ if __name__ == "__main__":
                             metadata_dict[key].append(Xd.meta[key])
                     
                     # Calculate current radius and resize
+                    Xd.meta['RSUN_OBS'] = 949.80
+                    Xd.meta['CDELT1']=0.504
                     current_radius = Xd.meta['RSUN_OBS'] / Xd.meta['CDELT1']
                     X, scale_factor = resize_to_match_solar_radius(
                         Xd.data, current_radius, TARGET_RADIUS, padding_value=0
@@ -274,6 +346,7 @@ if __name__ == "__main__":
                     X[np.where(X <= 0.0)] = 0.0
                     
                     # Apply similarity transform (using TARGET_RADIUS as reference)
+                    #rad = Xd.meta['RSUN_OBS']
                     rad = Xd.meta['RSUN_OBS']
                     scale_factor = trgtAS / rad
                     t = (X.shape[0]/2.0) - scale_factor * (X.shape[0]/2.0)
@@ -289,72 +362,74 @@ if __name__ == "__main__":
                     print(f"Error processing Ic_noLimbDark file {file}: {e}")
                     continue
                 
-        # Process Magnetogram files with resizing
-        if args.do_M_list:
-            print("Processing Magnetogram files...")
-            metadata_dict = {}
-            
-            for fn, file in tqdm(enumerate(M_list), desc="Processing Magnetograms"):
-                try:
-                    data, header = fits.getdata(file, header=True)
-                    data = np.nan_to_num(data, nan=-5000)
-                    data = np.rot90(data, 2)
-                    
-                    header['cunit1'] = 'arcsec'
-                    header['cunit2'] = 'arcsec'
-                    header_string = header.tostring()
-                    header = fits.Header.fromstring(header_string)
-                    Xd = Map(data, header)
-                    
-                    # Store metadata
-                    Xd.meta.pop('license', None)
-                    for key in Xd.meta:
-                        if key not in ['keycomments', 'simple']:
-                            if key not in metadata_dict:
-                                metadata_dict[key] = []
-                            metadata_dict[key].append(Xd.meta[key])
-                    
-                    # Calculate current radius and resize
-                    current_radius = Xd.meta['RSUN_OBS'] / Xd.meta['CDELT1']
-                    X, scale_factor = resize_to_match_solar_radius(
-                        Xd.data, current_radius, TARGET_RADIUS, padding_value=-5000
-                    )
-                    
-                    # Apply existing transformations
-                    validMask = np.isfinite(X) 
-                    validMask = validMask.astype(float)
-                    
-                    padding_value = -5000  
-                    X_processed = X.copy()
-                    X_processed[~validMask.astype(bool)] = padding_value
-                    
-                    rad = Xd.meta['RSUN_OBS']
-                    scale_factor = trgtAS / rad
-                    t = (X_processed.shape[0]/2.0) - scale_factor * (X_processed.shape[0]/2.0)
-                    XForm = skimage.transform.SimilarityTransform(scale=scale_factor, translation=(t,t))
-                    
-                    Xr = skimage.transform.warp(X_processed, XForm.inverse, preserve_range=True, 
-                                            mode='edge', output_shape=(X_processed.shape[0], X_processed.shape[0]))
-                    Xm = skimage.transform.warp(validMask, XForm.inverse, preserve_range=True, 
-                                            mode='edge', output_shape=(X_processed.shape[0], X_processed.shape[0]))
-                    mask_threshold = 0.1
-                    valid_correction = Xm > mask_threshold
-                    
-                    Xr_corrected = Xr.copy()
-                    Xr_corrected[valid_correction] = Xr[valid_correction] / Xm[valid_correction]
-                    
-                    Xr_corrected[~valid_correction] = padding_value
-                    
-                    Xr_corrected = skimage.transform.downscale_local_mean(Xr_corrected, (divideFactor, divideFactor))
-                    Xr_corrected = Xr_corrected.astype('float32')
-                    M[fn,:,:] = Xr_corrected
-                    
-                except Exception as e:
-                    print(f"Error processing magnetogram file {file}: {e}")
-                    continue
-            else:
-                print("No Magnetogram files found for this year.")
                 
+        # Process Magnetogram files with resizing
+        
+        print("Processing Magnetogram files...")
+        metadata_dict = {}
+        
+        for fn, file in tqdm(enumerate(M_list), desc="Processing Magnetograms"):
+            try:
+                data, header = fits.getdata(file, header=True)
+                data = np.nan_to_num(data, nan=-5000)
+                data = np.rot90(data, 2)
+                
+                header['cunit1'] = 'arcsec'
+                header['cunit2'] = 'arcsec'
+                header_string = header.tostring()
+                header = fits.Header.fromstring(header_string)
+                Xd = Map(data, header)
+                
+                # Store metadata
+                Xd.meta.pop('license', None)
+                for key in Xd.meta:
+                    if key not in ['keycomments', 'simple']:
+                        if key not in metadata_dict:
+                            metadata_dict[key] = []
+                        metadata_dict[key].append(Xd.meta[key])
+                
+                # Calculate current radius and resize
+                Xd.meta['RSUN_OBS'] = 949.80
+                Xd.meta['CDELT1']=0.504
+                current_radius = Xd.meta['RSUN_OBS'] / Xd.meta['CDELT1']
+                X, scale_factor = resize_to_match_solar_radius(
+                    Xd.data, current_radius, TARGET_RADIUS, padding_value=-5000
+                )
+                
+                # Apply existing transformations
+                validMask = np.isfinite(X) 
+                validMask = validMask.astype(float)
+                
+                padding_value = -5000  
+                X_processed = X.copy()
+                X_processed[~validMask.astype(bool)] = padding_value
+                
+                rad = Xd.meta['RSUN_OBS']
+                scale_factor = trgtAS / rad
+                t = (X_processed.shape[0]/2.0) - scale_factor * (X_processed.shape[0]/2.0)
+                XForm = skimage.transform.SimilarityTransform(scale=scale_factor, translation=(t,t))
+                
+                Xr = skimage.transform.warp(X_processed, XForm.inverse, preserve_range=True, 
+                                        mode='edge', output_shape=(X_processed.shape[0], X_processed.shape[0]))
+                Xm = skimage.transform.warp(validMask, XForm.inverse, preserve_range=True, 
+                                        mode='edge', output_shape=(X_processed.shape[0], X_processed.shape[0]))
+                mask_threshold = 0.1
+                valid_correction = Xm > mask_threshold
+                
+                Xr_corrected = Xr.copy()
+                Xr_corrected[valid_correction] = Xr[valid_correction] / Xm[valid_correction]
+                
+                Xr_corrected[~valid_correction] = padding_value
+                
+                Xr_corrected = skimage.transform.downscale_local_mean(Xr_corrected, (divideFactor, divideFactor))
+                Xr_corrected = Xr_corrected.astype('float32')
+                M[fn,:,:] = Xr_corrected
+                
+            except Exception as e:
+                print(f"Error processing magnetogram file {file}: {e}")
+                continue
+            
+
                 
         # Process AIA files (existing logic)
         for dataset_name, file_list in filelists.items():
@@ -411,6 +486,7 @@ if __name__ == "__main__":
                     except Exception as e:
                         print(f"Error processing AIA file {file}: {e}")
                         continue
+                    
                 # Store attributes for AIA datasets
                 dataset = aia_datasets[dataset_name]
                 for key, values in metadata_dict.items():
